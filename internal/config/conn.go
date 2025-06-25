5b37643330ef347b03373676b3045053dbee6da6
Jean Carlos <jean@naxostelecom.com.br>
2025-06-25 17:31:07 -0300

    refactor: refatorado função para conectar ao banco

--- internal/config/conn.go
+++ internal/config/conn.go
@@ -3,22 +3,46 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type config struct {
+	dbUser     string
+	dbPassword string
+	dbHost     string
+	dbPort     string
+	dbName     string
+}
+
+func conn() config {
+	return config{
+		dbUser:     "userdb",
+		dbPassword: "1234",
+		dbHost:     "localhost",
+		dbPort:     "3306",
+		dbName:     "vod_db",
+	}
+}
+
 func ConnectDB() (*sql.DB, error) {
-	fmt.Println("Connecting to the database...")
-	dsn := "userdb:1234@tcp(localhost:3306)/vod_db"
-	db, err := sql.Open("mysql", dsn)
+	config := conn()
+
+	conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		config.dbUser, config.dbPassword, config.dbHost, config.dbPort, config.dbName)
+	db, err := sql.Open("mysql", conn)
 
 	if err != nil {
+		log.Printf("Erro ao abrir a conexão: %v\n", err)
 		panic(err)
 	}
 
 	if err := db.Ping(); err != nil {
+		log.Printf("Erro ao conectar ao banco: %v\n", err)
 		return nil, err
 	}
 
+	log.Println("Conexão com o banco de dados estabelecida com sucesso.")
 	return db, nil
 }
